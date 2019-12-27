d47717ef6c33a02f5f8ae75e4e5c1d900e80df76
hamed-afshar <hamed.afshar.n@gmail.com>
2019-12-28 01:22:58 +0330

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/svg/logo.jpg" alt="logo" style="max-height: 200px; max-width: 200px" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong>post</div>
+                <div class="pr-5"><strong>16k</strong>followers</div>
+                <div class="pr-5"><strong>212</strong>following</div>
+            </div>
+            <div class="pt-4 font-weight-bold"> freeCodeCamp.org</div>
+            <div class="">We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fmct2-1.fna.fbcdn.net/v/t51.2885-15/e35/c94.0.561.561a/s150x150/79225007_186012015855932_2310000774829668783_n.jpg?_nc_ht=instagram.fmct2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=z7Nhbq_GLwAAX_X2dqb&oh=5095f8bd8faeb4f5d1f18f6f0da9fadf&oe=5EA996B1" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fmct2-1.fna.fbcdn.net/v/t51.2885-15/e35/c94.0.561.561a/s150x150/79225007_186012015855932_2310000774829668783_n.jpg?_nc_ht=instagram.fmct2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=z7Nhbq_GLwAAX_X2dqb&oh=5095f8bd8faeb4f5d1f18f6f0da9fadf&oe=5EA996B1" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fmct2-1.fna.fbcdn.net/v/t51.2885-15/e35/c94.0.561.561a/s150x150/79225007_186012015855932_2310000774829668783_n.jpg?_nc_ht=instagram.fmct2-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=z7Nhbq_GLwAAX_X2dqb&oh=5095f8bd8faeb4f5d1f18f6f0da9fadf&oe=5EA996B1" class="w-100">
+        </div>
+    </div>
+
+</div>
+@endsection
